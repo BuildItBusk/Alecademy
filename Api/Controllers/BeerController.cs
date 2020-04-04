4bb77428a0c178ad306ac78dec37c314473f200c
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2020-04-04 12:48:41 +0200

    API added. Including the beer controller.

---
+++ Api/Controllers/BeerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private readonly ILogger<BeerController> _logger;
+
+        public BeerController(ILogger<BeerController> logger)
+        {
+            _logger = logger;            
+        }
+        
+        [HttpGet]
+        public Beer Get()
+        {
+            var rng = new Random();
+            _logger.LogInformation("User requested a random beer.");
+            return Beers[rng.Next(Beers.Count)];
+        }
+
+        private readonly List<Beer> Beers = new List<Beer>
+        {
+            new Beer { Name = "Tuborg Classic", AlcoholVol = 5.6m, Origin = new Country { Name = "Denmark" }},
+            new Beer { Name = "Grimbergen Blonde", AlcoholVol = 6.7m, Origin = new Country { Name = "France" }}
+        };
+    }
+}
---
+++ Api/Models/Beer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Beer
+    {
+        public int Id { get; }
+
+        public string Name { get; set; }
+
+        public decimal AlcoholVol { get; set; }
+
+        public Country Origin { get; set; }
+
+        public Uri ImageUri { get; set; }
+    }
+}
