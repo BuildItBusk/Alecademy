4b10651808f548ed63d9003a6f0c602eb54ebfd1
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2020-07-03 19:53:22 +0200

    Added a controller and VS Code files

---
+++ Api/Controllers/BrewryController.cs
@@ -0,0 +1,29 @@
+﻿using Api.Data.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class BrewryController : ControllerBase
+    {
+        private ILogger<BrewryController> _logger;
+        private SqlContext _db;
+
+        public BrewryController(ILogger<BrewryController> logger, SqlContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetRecent(int num = 5)
+        {
+            var breweries = _db.Breweries.OrderByDescending(b => b.Id).Take(num);
+            return Ok(breweries);
+        }
+    }
+}
